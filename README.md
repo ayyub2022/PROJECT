5666bda263e3c268cbc1dadcf69797b1ef411923
ayyub2022 <ayyubibrahim22@gmail.com>
2024-04-14 02:01:12 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# (PROJECT)
+
+#### Date, 2024/04/12
+
+#### By *Ayub Hussein*
+
+## Description
+This is my project that I have been working on.
+## Installation
+You use git clone to be able to download the documents in the GitHub
+
+## Installation Requirements
+Git
+
+### Installation instruction
+create repository
+Git clone 
+
+```
+
+# Live Link
+[Git](gh-pages link)
+https://github.com/ayyub2022/PROJECT.git
+## Technologies used
+HTML
+CSS
+Javascript
+
+## Support and contact details
+github.com/Ayub Hussein
+
+### License
+Copyright (c) 2024.
+
+
+
---
+++ index.js
@@ -0,0 +1,45 @@
+// Base URL for the Quran API
+const apiUrlBase = "http://api.alquran.cloud/v1/ayah/";
+
+// Reference to the HTML element where the Quran verse will be displayed
+const quote = document.getElementById("verseDisplay");
+
+// Function to generate a random Ayah number
+function getRandomAyahNumber() {
+    // The Quran has 6236 verses in total
+    return Math.floor(Math.random() * 6236) + 1;
+}
+
+// Function to fetch a random Ayah asynchronously and display it
+async function getRandomVerse() {
+    // Generate a random Ayah number
+    const ayahNumber = getRandomAyahNumber();
+
+    // Choose the edition for the translation (e.g., 'en.asad' for Asad translation in English)
+    const edition = 'en.asad'; 
+
+    // Construct the URL for fetching the Ayah
+    const url = `${apiUrlBase}${ayahNumber}/${edition}`;
+    try {
+        // Fetch the Ayah data from the API
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        
+        // Check if the response is successful
+        if (data.code === 200 && data.status === 'OK') {
+            // Display the text of the Ayah in the HTML element
+            quote.innerHTML = data.data.text;
+        } else {
+            // Display an error message if fetching the Ayah fails
+            quote.innerHTML = "An error occurred fetching the Ayah.";
+        }
+    } catch (error) {
+        // Log and display an error message if an error occurs during fetching
+        console.error('Error fetching the Ayah:', error);
+        quote.innerHTML = "An error occurred fetching the Ayah.";
+    }
+}
+
+// Call the function to fetch and display a random verse when the page loads
+getRandomVerse();
